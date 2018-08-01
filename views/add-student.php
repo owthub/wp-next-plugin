3ab1763f651da4067ade01dcfd6bf009bde1a248
Online Web Tutor <onlinewebtutorhub@gmail.com>
2018-08-01 08:32:56 +0530

    Edit details & delete

--- views/add-student.php
+++ views/add-student.php
@@ -1,33 +1,65 @@
 <?php
 global $wpdb;
 $msg = '';
-if (isset($_POST['btnsubmit'])) {
-    $wpdb->insert("wp_next_plugin_tbl", array(
-        "name" => $_POST['txtname'],
-        "email" => $_POST['txtemail']
-    ));
 
-    if ($wpdb->insert_id > 0) {
-        $msg = "Form data saved successfully";
+$action = isset($_GET['action']) ? trim($_GET['action']) : "";
+$id = isset($_GET['id']) ? intval($_GET['id']) : "";
+
+$row_details = $wpdb->get_row(
+        $wpdb->prepare(
+                "SELECT * from wp_next_plugin_tbl WHERE id = %d", $id
+        ), ARRAY_A
+);
+
+
+if (isset($_POST['btnsubmit'])) {
+
+    $action = isset($_GET['action']) ? trim($_GET['action']) : "";
+    $id = isset($_GET['id']) ? intval($_GET['id']) : "";
+
+    if (!empty($action)) {
+
+        $wpdb->update("wp_next_plugin_tbl", array(
+            "name" => $_POST['txtname'],
+            "email" => $_POST['txtemail']
+                ), array(
+            "id" => $id
+        ));
+
+        $msg = "Form data updated successfully";
     } else {
-        $msg = "Failed to save data";
+
+        $wpdb->insert("wp_next_plugin_tbl", array(
+            "name" => $_POST['txtname'],
+            "email" => $_POST['txtemail']
+        ));
+
+        if ($wpdb->insert_id > 0) {
+            $msg = "Form data saved successfully";
+        } else {
+            $msg = "Failed to save data";
+        }
     }
 }
 ?>
 
 <p><?php echo $msg; ?></p>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>?page=wp-next-add" method="post">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>?page=wp-next-add<?php
+if (!empty($action)) {
+    echo '&action=edit&id=' . $id;
+}
+?>" method="post">
     <p>
         <label>
             Name
         </label>
-        <input type="text" name="txtname" placeholder="Enter name"/>
+        <input type="text" name="txtname" value="<?php echo isset($row_details['name']) ? $row_details['name'] : ""; ?>" placeholder="Enter name"/>
     </p>
     <p>
         <label>
             Email
         </label>
-        <input type="email" name="txtemail" placeholder="Enter email"/>
+        <input type="email" name="txtemail" value="<?php echo isset($row_details['email']) ? $row_details['email'] : ""; ?>" placeholder="Enter email"/>
     </p>
     <p>
         <button type="submit" name="btnsubmit">Submit</button>
--- views/list-student.php
+++ views/list-student.php
@@ -5,6 +5,28 @@ $all_students = $wpdb->get_results(
                 "SELECT * from wp_next_plugin_tbl", ""
         ), ARRAY_A
 );
+
+$action = isset($_GET['action']) ? trim($_GET['action']) : "";
+$id = isset($_GET['id']) ? intval($_GET['id']) : "";
+if (!empty($action) && $action == "delete") {
+
+    $row_exists = $wpdb->get_row(
+            $wpdb->prepare(
+                    "SELECT * from wp_next_plugin_tbl WHERE id = %d", $id
+            )
+    );
+    if (count($row_exists) > 0) {
+        $wpdb->delete("wp_next_plugin_tbl", array(
+            "id" => $id
+        ));
+    }
+    ?>
+    <script>
+        location.href = "<?php echo site_url() ?>/wp-admin/admin.php?page=wp-next-plugin";
+    </script>
+    <?php
+}
+
 if (count($all_students) > 0) {
     ?>
     <table cellpadding="10" border="1">
@@ -12,6 +34,7 @@ if (count($all_students) > 0) {
             <th>Sr No</th>
             <th>Name</th>
             <th>Email</th>
+            <th>Action</th>
         </tr>
         <?php
         $count = 1;
@@ -21,6 +44,10 @@ if (count($all_students) > 0) {
                 <td><?php echo $count++; ?></td>
                 <td><?php echo $student['name'] ?></td>
                 <td><?php echo $student['email'] ?></td>
+                <td>
+                    <a href="admin.php?page=wp-next-add&action=edit&id=<?php echo $student['id']; ?>">Edit</a> 
+                    <a href="admin.php?page=wp-next-plugin&id=<?php echo $student['id']; ?>&action=delete" onclick="return confirm('Are you sure want to delete?')">Delete</a>
+                </td>
             </tr>
             <?php
         }
